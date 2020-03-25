d5e48ed3c84c6a2fb103b4b8484514a28b8e5554
isaacweintraub@gmail.com <isaacweintraub@gmail.com>
2020-03-25 17:19:58 -0400

    fixed the percentage bug and indexing bug for the states

--- plotData.m
+++ plotData.m
@@ -10,9 +10,13 @@ function [] = plotData (Name,pop,cases,deaths,recovered)
 %--------------------------------------------------------------------------
 figure;
 hold on
-H1=area(0:100,1.0*ones(size(0:100)),'FaceColor',[1 0 0],...
+H1=area(0:100,100*ones(size(0:100)),'FaceColor',[1 0 0],...
     'FaceAlpha',0.6,'EdgeColor','none');
-H1=area(0:100,0.10*ones(size(0:100)),'FaceColor',[1 1 1],...
+H1=area(0:100,10*ones(size(0:100)),'FaceColor',[1 1 1],...
+    'FaceAlpha',0.2,'EdgeColor','none');
+H1=area(0:100,1*ones(size(0:100)),'FaceColor',[1 1 1],...
+    'FaceAlpha',0.2,'EdgeColor','none');
+H1=area(0:100,0.1*ones(size(0:100)),'FaceColor',[1 1 1],...
     'FaceAlpha',0.2,'EdgeColor','none');
 H1=area(0:100,0.01*ones(size(0:100)),'FaceColor',[1 1 1],...
     'FaceAlpha',0.2,'EdgeColor','none');
@@ -22,25 +26,21 @@ H1=area(0:100,0.0001*ones(size(0:100)),'FaceColor',[1 1 1],...
     'FaceAlpha',0.2,'EdgeColor','none');
 H1=area(0:100,0.00001*ones(size(0:100)),'FaceColor',[1 1 1],...
     'FaceAlpha',0.2,'EdgeColor','none');
-H1=area(0:100,0.000001*ones(size(0:100)),'FaceColor',[1 1 1],...
-    'FaceAlpha',0.2,'EdgeColor','none');
-H1=area(0:100,0.0000001*ones(size(0:100)),'FaceColor',[1 1 1],...
-    'FaceAlpha',0.2,'EdgeColor','none');
-text(2.5,1.6,['100% Pop: ' num2str(1.0*pop)])
-text(2.5,0.15,['10% Pop: ' num2str(0.1*pop)])
-text(2.5,0.015,['1% Pop: ' num2str(0.01*pop)])
-text(2.5,0.0015,['0.1% Pop: ' num2str(0.001*pop)])
-text(2.5,0.00015,['0.01% Pop: ' num2str(0.0001*pop)])
-text(length(cases)+3,cases(end)./pop,['Cases: ' num2str(cases(end))])
-text(length(deaths)+3,deaths(end)./pop,['Deaths: ' num2str(deaths(end))])
-p1 = semilogy(cases./pop,'ko','DisplayName',[Name ' Confirmed Cases'],'MarkerSize',8);
-p2 = semilogy(deaths./pop,'ks','DisplayName',[Name ' Confirmed Deaths'],'MarkerSize',8);
-p3 = semilogy(recovered./pop,'kp','DisplayName',[Name ' Confirmed Recovery'],'MarkerSize',10);
+text(2.5,160,['100% Pop: ' num2str(100*pop)])
+text(2.5,15,['10% Pop: ' num2str(10*pop)])
+text(2.5,01.5,['1% Pop: ' num2str(1*pop)])
+text(2.5,0.15,['0.1% Pop: ' num2str(0.1*pop)])
+text(2.5,0.015,['0.01% Pop: ' num2str(0.01*pop)])
+text(length(cases)+3,100*cases(end)./pop,['Cases: ' num2str(cases(end))])
+text(length(deaths)+3,100*deaths(end)./pop,['Deaths: ' num2str(deaths(end))])
+p1 = semilogy(100*cases./pop,'ko','DisplayName',[Name ' Confirmed Cases'],'MarkerSize',8);
+p2 = semilogy(100*deaths./pop,'ks','DisplayName',[Name ' Confirmed Deaths'],'MarkerSize',8);
+p3 = semilogy(100*recovered./pop,'kp','DisplayName',[Name ' Confirmed Recovery'],'MarkerSize',10);
 ffig = gcf;
 ffig.Children.YScale = 'log';
 legend([p1 p2 p3],'location','NorthEast')
 xlim([0 100])
-ylim([1e-8,1])
+ylim([1e-6,1e2])
 grid on
 title(Name)
 ylabel('Percent of total Population, %')
