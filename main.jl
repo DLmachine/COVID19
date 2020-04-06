3c68903be7be60f7b6c83126a6cf3f0afee532d5
Isaac Weintraub <isaacweintraub@gmail.com>
2020-04-05 21:13:02 -0400

    Julia Code Up and Running to Draw States

--- main.jl
+++ main.jl
@@ -7,32 +7,34 @@
 # REQ.PACKAGES:	CSV
 # 
 #
-#-----------------------------------------------------------
-using CSV
-using Plots
-using DataFrames
-using LaTeXStrings
-using ProgressMeter
-plotly()
-# Read the csv files into julia memory
-popStates = CSV.read("USA.csv",header=true)
-casesUSA   = CSV.read("COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv",header=true)
-# Run a for loop though each state
-casesUSAstateList = casesUSA[:,7]
-n = size(popStates)[1]
-@showprogress 0.01 for i = 1:n
-#i = 1
-state = popStates[i,1]
-pop = popStates[i,2]
-index = casesUSA[:,7].==state
-stateData = casesUSA[index,12:end]
-M = convert(Array, stateData[1:end,:])
-stateCases = sum(M,dims=1)
-plot()
-plot!(1:size(stateCases)[2],100*stateCases[1,:]./pop,lab=state, scale=:log10,yticks = [1e-6,1e-5,1e-4,1e-3,1e-2,1e-1,1,1e1,1e2],yaxis="Percent of State", xaxis="Days since Jan 22")
-p = plot!(xscale=:identity,xticks=0:10:100)
-xlims!((0,100))
-ylims!((1e-6,100))
-png(p,"StateFigures/$state.png")
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+run(`git fetch`)    # Get the latest data
+using CSV           # Need to read the CSV data
+using Plots         # Allows the genation of plots
+using DataFrames    # Used for handliong the csv file (contains strings and numbers)
+using ProgressMeter # Used for making the progress meeter
+pyplot()            # Use the python plotting backend
+popStates = CSV.read("USA.csv",header=true)     # Read the population data from the united states
+casesUSA   = CSV.read("COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv",header=true) # Read the latests data from JOHNS HOPKINS
+casesUSAstateList = casesUSA[:,7]       # Gather a list of all the states
+n = size(popStates)[1]                  # the number of provinces and states of the USA
+@showprogress 0.01 for i = 1:n          # show a progress meter and for loop through the states
+    state = popStates[i,1]              # get the name of the current state (STRING)
+    pop = popStates[i,2]                # get the population of the current state (FLOAT)
+    index = casesUSA[:,7].==state       # get the subset of indexies whicih corespond to data for that state
+    stateData = casesUSA[index,12:end]  # parse the subset of the data for the state of interest
+    M = convert(Array, stateData[1:end,:])  # Conver the data to a matrix for manipulation reasons
+    stateCases = sum(M,dims=1)          # summing is easier as a matrix than as a data frame NEED THE TOTAL NUMBER OF CASES in each state
+    # Plot the current state or provices
+    plot()
+    plot!(1:size(stateCases)[2],
+        100*stateCases[1,:]./pop,lab=state,
+        scale=:log10,
+        yticks = [1e-6,1e-5,1e-4,1e-3,1e-2,1e-1,1,1e1,1e2],
+        yaxis="Percent of State", xaxis="Days since Jan 22")
+    p = plot!(xscale=:identity,xticks=0:10:100)
+    xlims!((0,100))
+    ylims!((1e-6,100))
+    png(p,"StateFigures/$state.png")
 end
 print("Done")
--- mainCOVID19.jl
+++
@@ -1,8 +0,0 @@
-# Julia COVID 19 Data Analusis
-# Isaac Weintraub MAR 2020
-
-#;`git submodule add https://github.com/CSSEGISandData/COVID-19.git`
-#;`git submodule add https://github.com/datasets/population.git`
-;`git submodule update --remote`
-Pkg.add("CSV")
-pops = CSV.File(population/data/population);
